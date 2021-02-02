199d02c38fe6be22d480236b2284e70807800bd2
Micah Bowie <micahbowie20@gmail.com>
2021-02-01 22:41:14 -0500

    created equipmentitem logic.

--- frontend/v1/src/index.js
+++ frontend/v1/src/index.js
@@ -1,9 +1,10 @@
 const api = new API();
 document.addEventListener("DOMContentLoaded", () => {
-  api.fetchManager(1)
-  api.fetchEmployees()
+  // api.fetchManager(1)
+  // api.fetchEmployees()
   api.fetchEquipmentItems()
-  createEmployeeForm()
+  // createEmployeeForm()
+  createEquipmentItemForm()
 })
 
 
@@ -91,3 +92,66 @@ for (const button of buttons){
 
 
 // Equipment Item //
+
+function createEquipmentItemForm() {
+  let equipmentItemForm = document.getElementById("create-equipment-item-container")
+  equipmentItemForm.innerHTML +=
+
+  `
+  <h3>Add new equipment: </h3>
+  <form>
+    <label for="name">Name:</label>
+     <input type="text" id="name" name="name"><br>
+    <label for="equipment_type">Equipment Type:</label>
+     <input type="text" id="equipment_type" name="equipment_type"><br>
+    <label for="warranty">Warranty:</label>
+     <input type="text" id="warranty" name="warranty"><br>
+    <label for="manufacture_id">Manufacture Id:</label>
+     <input type="text" id="manufacture_id" name="manufacture_id"><br>
+    <label for="notes">Notes:</label>
+     <input type="text" id="notes" name="notes"><br>
+    <label for="pack_id">Pack Id:</label>
+     <input type="text" id="pack_id" name="pack_id"><br>
+    <label for="specs">Specs:</label>
+     <input type="text" id="specs" name="specs"><br>
+     <input type="submit" id="submit-button" value="Save">
+  </form>
+  `
+  equipmentItemForm.addEventListener("submit", equipmentItemFormSubmission);
+
+}
+
+
+function equipmentItemFormSubmission() {
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let equipmentType = document.getElementById("equipment_type").value;
+  let warranty = document.getElementById("warranty").value;
+  let manufactureId = document.getElementById("manufacture_id").value;
+  let notes = document.getElementById("notes").value;
+  let packId = document.getElementById("pack_id").value;
+  let specs = document.getElementById("specs").value;
+  console.log("About to create item")
+  let equipmentItem = {
+    name: name,
+    equipmentType: equipmentType,
+    warranty: warranty,
+    manufactureId: manufactureId,
+    notes: notes,
+    packId: packId,
+    specs: specs
+  }
+
+  console.log("Item created")
+  fetch("http://localhost:3000/api/v1/equipment_items", {
+    method: "POST",
+    headers: {"Accepts": "application/json", "Content-Type": "application/json"},
+    body: JSON.stringify(equipmentItem)
+  })
+  .then(resp => resp.json())
+  .then(equipmentItem => {
+    let item = new EquipmentItem(equipmentItem.id, equipmentItem.name, equipmentItem.equipment_type, equipmentItem.warranty, equipmentItem.manufacture_id, equipmentItem.notes, equipmentItem.pack_id, equipmentItem.specs, equipmentItem.unique_id, equipmentItem.manual, equipmentItem.picture)
+    console.log("Just before render")
+    item.renderEquipmentItem();
+  })
+}
