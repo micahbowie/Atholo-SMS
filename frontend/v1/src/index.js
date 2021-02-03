32f3c700a30a03f854aa4a7d330b46c0ac8528c9
Micah Bowie <micahbowie20@gmail.com>
2021-02-02 22:49:04 -0500

    updated index.js with contact form functions

--- frontend/v1/src/index.js
+++ frontend/v1/src/index.js
@@ -1,7 +1,10 @@
 const api = new API();
 document.addEventListener("DOMContentLoaded", () => {
-  // api.fetchManager(1)
-  api.fetchEmployees()
+  api.fetchManager(1)
+  // api.fetchEmployees()
+  // api.fetchContacts()
+  // createContactForm()
+  // api.fetchContact(3)
   // api.fetchEquipmentItems()
   // createEmployeeForm()
   // createEquipmentItemForm()
@@ -153,4 +156,97 @@ function equipmentItemFormSubmission() {
     let item = new EquipmentItem(equipmentItem.id, equipmentItem.name, equipmentItem.equipment_type, equipmentItem.warranty, equipmentItem.manufacture_id, equipmentItem.notes, equipmentItem.pack_id, equipmentItem.specs, equipmentItem.unique_id, equipmentItem.manual, equipmentItem.picture)
     item.renderEquipmentItem();
   })
+}
+
+
+
+
+// Contact //
+
+function createContactForm() {
+  let contactForm = document.getElementById("create-contact-container")
+  contactForm.innerHTML +=
+
+  `
+  <h3>Add a new contact: </h3>
+  <form>
+    <label for="first_name">First name:</label>
+     <input type="text" id="first_name" name="last_name"><br>
+    <label for="last_name">Last name:</label>
+     <input type="text" id="last_name" name="last_name"><br>
+    <label for="company">Company:</label>
+     <input type="text" id="company" name="company"><br>
+     <label for="department">Department:</label>
+      <input type="text" id="department" name="department"><br>
+    <label for="job_title">Job title:</label>
+     <input type="text" id="job_title" name="job_title"><br>
+    <label for="school">School:</label>
+     <input type="text" id="school" name="school"><br>
+    <label for="phone">Phone:</label>
+     <input type="text" id="phone" name="phone"><br>
+    <label for="email">Email:</label>
+     <input type="text" id="email" name="email"><br>
+     <label for="alt_phone">Alt Phone:</label>
+      <input type="text" id="alt_phone" name="alt_phone"><br>
+     <label for="alt_email">Alt Email:</label>
+      <input type="text" id="alt_email" name="alt_email"><br>
+    <label for="address">Adress:</label>
+     <input type="text" id="address" name="address"><br>
+     <label for="website">Website:</label>
+      <input type="text" id="website" name="website"><br>
+     <label for="notes">Notes:</label>
+      <input type="text" id="notes" name="notes"><br>
+     <input type="submit" value="Save">
+  </form>
+  `
+
+  contactForm.addEventListener("submit", contactFormSubmission);
+
+}
+
+
+function contactFormSubmission() {
+  event.preventDefault();
+  let firstName = document.getElementById("first_name").value;
+  let lastName = document.getElementById("last_name").value;
+  let phone = document.getElementById("phone").value;
+  let altPhone = document.getElementById("alt_phone").value;
+  let email = document.getElementById("email").value;
+  let altEmail = document.getElementById("alt_email").value;
+  let company = document.getElementById("company").value;
+  let website = document.getElementById("website").value;
+  let jobTitle = document.getElementById("job_title").value;
+  let department = document.getElementById("department").value;
+  let address = document.getElementById("address").value;
+  let notes = document.getElementById("notes").value;
+  let school = document.getElementById("school").value;
+
+  let contact = {
+    first_name: firstName,
+    last_name: lastName,
+    phone: phone,
+    alt_phone: altPhone,
+    email:email,
+    alt_email: altEmail,
+    company: company,
+    website: website,
+    job_title: jobTitle,
+    department: department,
+    address: address,
+    notes: notes,
+    school: school
+  }
+
+
+  fetch("http://localhost:3000/api/v1/contacts", {
+    method: "POST",
+    headers: {"Accepts": "application/json", "Content-Type": "application/json"},
+    body: JSON.stringify(contact)
+  })
+  .then(response => response.json())
+  .then(contact => {
+    let contct = new Contact(contact.id, contact.first_name, contact.last_name, contact.phone, contact.alt_phone, contact.email, contact.alt_email, contact.company, contact.website, contact.job_title, contact.department, contact.address, contact.notes, contact.school, contact.school_id)
+    contct.renderContact();
+  })
+
 }
